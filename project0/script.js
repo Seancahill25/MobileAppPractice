09cea8b0ee675964344494bfe96c8867f714130e
Sean Cahill <seancahill25@gmail.com>
2018-08-21 03:55:40 +0000

    now has checkboxes and counts todos

--- project0/script.js
+++ project0/script.js
@@ -11,15 +11,22 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 
 function newTodo() {
     var li = document.createElement("li");
-    var todo = document.createTextNode("todo");
+    var Todo = document.createTextNode("Todo");
     var erase = document.createElement("BUTTON");
+    var check = document.createElement("INPUT");
     var txt = document.createTextNode("x");
+    var counter = itemCountSpan.innerHTML;
     list.appendChild(li);
-    li.appendChild(todo);
+    li.appendChild(Todo);
     erase.appendChild(txt);
     li.appendChild(erase);
+    check.setAttribute("type", "checkbox");
+    li.appendChild(check);
+    counter++;
+    itemCountSpan.innerHTML = counter;
 }
 
 function deleteTodo() {
+    document.getElementsByTagName('li');
 
 }
